cedd72ba9899aeb9daf8fa88982262d1e672141e
Yang Jie <jie1.yang@icloud.com>
2024-02-21 19:59:01 +0800

    feat: refactor API module and add missing semicolons

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 import api from "./api";
 
-
 //获取导航栏
-export const reqMenuData = () => api.get('/menu')
+export const reqMenuData = () => api.get("/menu");
 
 //产品
-export const reqProductById = (id) => api.get(`/product/detail?id=${id}`)
+export const reqProductById = (id) => api.get(`/product/detail?id=${id}`);
 
 //img
-export const reqImgList = (type,productId = -1) => api.get(`/img?type=${type}&productId=${productId}`)
+export const reqImgList = (type, productId = -1) =>
+  api.get(`/img?type=${type}&productId=${productId}`);
 
 // 获取产品页数据
-export const reqLevel2Products = (id) => api.get(`/product/level2?id=${id}`)
+export const reqLevel2Products = (id) => api.get(`/product/level2?id=${id}`);
 // 传入三级导航对应产品  返回二级导航id
-export const getParent = (id) => api.get(`/menu/getParent?id=${id}`)
+export const getParent = (id) => api.get(`/menu/getParent?id=${id}`);
